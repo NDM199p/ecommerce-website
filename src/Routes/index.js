3e329e025418f83014c29aa067108a846dd8d322
Your Name <you@example.com>
2024-01-03 08:41:14 +0700

    update 1

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,11 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "../Pages/Home";
 import ProductDetailsPage from "../Pages/ProductDetails";
+import CartPage from "../Pages/CartPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <HomePage />,
+  },
+
+  {
+    path: "/cart-page",
+    element: <CartPage />,
   },
 
   {
